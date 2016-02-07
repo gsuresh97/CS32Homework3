dca468e9e1f4b2de9f8e209a33e33b6ec060501d
epsilonpie <suresh.gopikrishnan@gmail.com>
2016-02-07 11:42:38 -0800

    Some implemtation added

--- Landmark/Landmark/landmark.cpp
+++ Landmark/Landmark/landmark.cpp
@@ -1,15 +1,81 @@
-//
-//  main.cpp
-//  Landmark
-//
-//  Created by Gopi Suresh on 2/7/16.
-//  Copyright © 2016 Gopi. All rights reserved.
-//
-
 #include <iostream>
+#include <string>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+//Your declarations and implementations would go here
+class Landmark{
+public:
+    virtual string color() const{
+        return "yellow";
+    }
+    virtual string icon() const = 0;
+    virtual string name() const = 0;
+    
+};
+
+class Hotel: public Landmark{
+public:
+    Hotel(string n){
+        m_name = n;
+    }
+    virtual string name() const{
+        return m_name;
+    }
+    virtual string icon() const{
+        return "bed";
+    }
+private:
+    string m_name;
+    
+};
+
+class Restaurant: public Landmark{
+    
+};
+
+class Hospital:public Landmark{
+public:
+    Hospital(string n){
+        m_name = n;
+    }
+    virtual string name() const{
+        return m_name;
+    }
+    virtual string icon() const{
+        return "H";
+    }
+    virtual string color() const{
+        return "blue";
+    }
+private:
+    string m_name;
+
+    
+};
+
+void display(const Landmark* lm)
+{
+    cout << "Display a " << lm->color() << " " << lm->icon() << " icon for "
+    << lm->name() << "." << endl;
+}
+
+int main()
+{
+    Landmark* landmarks[4];
+    landmarks[0] = new Hotel("Westwood Rest Good");
+    // Restaurants have a name and seating capacity.  Restaurants with a
+    // capacity under 40 have a small knife/fork icon; those with a capacity
+    // 40 or over have a large knife/fork icon.
+    landmarks[1] = new Restaurant("Bruin Bite", 30);
+    landmarks[2] = new Restaurant("La Morsure de l'Ours", 100);
+    landmarks[3] = new Hospital("UCLA Medical Center");
+    
+    cout << "Here are the landmarks." << endl;
+    for (int k = 0; k < 4; k++)
+        display(landmarks[k]);
+    
+    // Clean up the landmarks before exiting
+    cout << "Cleaning up." << endl;
+    for (int k = 0; k < 4; k++)
+        delete landmarks[k];
 }
