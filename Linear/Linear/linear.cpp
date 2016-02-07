24f9d00bfc6141606179cd3425555d508b028566
Gopi Suresh <suresh.gopikrishnan@gmail.com>
2016-02-07 14:02:08 -0800

    anyTrue implemented correctly

--- Linear/Linear/linear.cpp
+++ Linear/Linear/linear.cpp
@@ -1,15 +1,76 @@
-//
-//  main.cpp
-//  Linear
-//
-//  Created by Gopi Suresh on 2/7/16.
-//  Copyright © 2016 Gopi. All rights reserved.
-//
-
 #include <iostream>
+#include <cmath>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+bool somePredicate(double x)
+{
+    return x < 0;
+}
+
+// Return true if the somePredicate function returns true for at
+// least one of the array elements, false otherwise.
+bool anyTrue(const double a[], int n)
+{
+    if(n==1)
+        return somePredicate(a[0]);
+    if(!somePredicate(a[n-1]))
+        return anyTrue(a, n-1);
+    return true;
+}
+
+// Return the number of elements in the array for which the
+// somePredicate function returns true.
+int countTrue(const double a[], int n)
+{
+    if (n==1)
+        return (int)(somePredicate(a[0]));
+    return somePredicate(a[n-1]) + countTrue(a, n-1);
+}
+
+// Return the subscript of the first element in the array for which
+// the somePredicate function returns true.  If there is no such
+// element, return -1.
+int firstTrue(const double a[], int n)
+{
+    return -999;  // This is incorrect.
+}
+
+// Return the subscript of the smallest element in the array (i.e.,
+// the one whose value is <= the value of all elements).  If more
+// than one element has the same smallest value, return the smallest
+// subscript of such an element.  If the array has no elements to
+// examine, return -1.
+int indexOfMin(const double a[], int n)
+{
+    return -999;  // This is incorrect.
+}
+
+// If all n2 elements of a2 appear in the n1 element array a1, in
+// the same order (though not necessarily consecutively), then
+// return true; otherwise (i.e., if the array a1 does not include
+// a2 as a not-necessarily-contiguous subsequence), return false.
+// (Of course, if a2 is empty (i.e., n2 is 0), return true.)
+// For example, if a1 is the 7 element array
+//    10 50 40 20 50 40 30
+// then the function should return true if a2 is
+//    50 20 30
+// or
+//    50 40 40
+// and it should return false if a2 is
+//    50 30 20
+// or
+//    10 20 20
+bool includes(const double a1[], int n1, const double a2[], int n2)
+{
+    return false;  // This is not always correct.
+}
+
+int main(){
+    const double list[] = {1, 2, 3, -4, -1, 2, 3};
+    /*if(countTrue(list, 7))
+        cout << "true"<< endl;
+    else
+        cout << "false" << endl;*/
+    
 }
