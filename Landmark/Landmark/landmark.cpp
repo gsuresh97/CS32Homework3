3b2ce0bfebda09ba7d503b3c7642685bba74c779
Gopi Suresh <suresh.gopikrishnan@gmail.com>
2016-02-08 20:48:00 -0800

    Landmark is submission ready

--- Landmark/Landmark/landmark.cpp
+++ Landmark/Landmark/landmark.cpp
@@ -1,8 +1,3 @@
-#include <iostream>
-#include <string>
-using namespace std;
-
-//Your declarations and implementations would go here
 class Landmark{
 public:
     Landmark(string n){
@@ -51,7 +46,6 @@ public:
     };
 private:
     int m_capacity;
-
 };
 
 class Hospital:public Landmark{
@@ -68,33 +62,4 @@ public:
     virtual ~Hospital(){
         cout << "Destroying the hospital " << name()<<".\n";
     }
-
-    
 };
-
-void display(const Landmark* lm)
-{
-    cout << "Display a " << lm->color() << " " << lm->icon() << " icon for "
-    << lm->name() << "." << endl;
-}
-
-int main()
-{
-    Landmark* landmarks[4];
-    landmarks[0] = new Hotel("Westwood Rest Good");
-    // Restaurants have a name and seating capacity.  Restaurants with a
-    // capacity under 40 have a small knife/fork icon; those with a capacity
-    // 40 or over have a large knife/fork icon.
-    landmarks[1] = new Restaurant("Bruin Bite", 30);
-    landmarks[2] = new Restaurant("La Morsure de l'Ours", 100);
-    landmarks[3] = new Hospital("UCLA Medical Center");
-    
-    cout << "Here are the landmarks." << endl;
-    for (int k = 0; k < 4; k++)
-        display(landmarks[k]);
-    
-    // Clean up the landmarks before exiting
-    cout << "Cleaning up." << endl;
-    for (int k = 0; k < 4; k++)
-        delete landmarks[k];
-}
